d270bfc9187813f90fb1ee719ad683931af2ff29
thm <thm@QWF5PB52K3.asiapacific.hpqcorp.net>
2024-03-09 11:08:18 +0530

    second commit

---
+++ SampleProject/src/main/java/DemoProject/simple.java
@@ -0,0 +1,5 @@
+package DemoProject;
+
+public class simple {
+
+}
---
+++ SampleProject/src/main/java/DemoProject/simpleClass.java
@@ -0,0 +1,14 @@
+package DemoProject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class simpleClass {
+
+	@Test
+	public void toLearnGitHub () {
+		Reporter.log("To Push GitHub", true);
+		
+	}
+	
+}
